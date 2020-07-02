1760ee02667de098e6453d8858b115ba484bf8d4
TimKHuang <timkhuang@icloud.com>
2020-07-02 19:35:24 +0800

    initialise the project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Minesweeper
+
+## About
+This is a platform providing the ability for people to build their
+own script to play the game *Minesweeper*. 
---
+++ src/main.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+@author: timkhuang
+@contact: timkhuang@icloud.com
+@software: PyCharm
+@file: main.py
+@time: 02/07/2020 19:32
+@description: This is the main file.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
