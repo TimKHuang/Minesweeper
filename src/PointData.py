a8c0e28f802f9cb6f6b49b3bbc9f237c55533060
TimKHuang <timkhuang@icloud.com>
2020-07-06 12:49:49 +0800

    Create board related mechanism

--- src/PointData.py
+++ src/PointData.py
@@ -38,14 +38,39 @@ class PointData:
         self.is_flagged = False
         self.bomb_around = 0
 
+    def set_bomb_around(self, number):
+        """
+        set the number of bombs around the current point.
+
+        Args:
+            number (int): number of bombs around
+        """
+        self.bomb_around = number
+
+    def continue_game(self):
+        """
+        True if the game continues.
+        False if meet a bomb and game over.
+        """
+        return not self.is_bomb
+
+    def open(self):
+        """
+        True if other points should be opened as well.
+        False if open should stop.
+        """
+        self.is_opened = True
+        return not self.is_bomb
+
     def __str__(self):
+        """ be able to print the point info. """
         return "Coordinate: ({}, {})" \
                "is_bomb: {}" \
                "is_opened: {}" \
                "is_flagged: {}" \
                "bomb_around: {}" \
-                .format(self.x, self.y,
-                        self.is_bomb,
-                        self.is_opened,
-                        self.is_flagged,
-                        self.bomb_around)
+            .format(self.x, self.y,
+                    self.is_bomb,
+                    self.is_opened,
+                    self.is_flagged,
+                    self.bomb_around)
