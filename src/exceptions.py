9378bcc313d5af7b13b278d9865a20f49ea15236
TimKHuang <timkhuang@icloud.com>
2020-07-07 16:26:52 +0800

    add comment for TooManyMineException

--- src/exceptions.py
+++ src/exceptions.py
@@ -7,6 +7,12 @@
 @time: 2020/7/7 下午3:57
 @description:
 """
+
+
 class TooManyMineException:
+    """
+    This is the exception called when trying to add to many exceptions.
+    """
+
     def __init__(self):
         super().__init__("Number of mines exceeds the board size")
