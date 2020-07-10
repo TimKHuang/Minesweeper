30a16f832e27e6efa28f7c9d2624bf0b8d19e9db
Victorvv <55767941+Victor-1024@users.noreply.github.com>
2020-07-10 13:25:02 +0800

    Adapting Class Point to Board.py

--- src/Board.py
+++ src/Board.py
@@ -16,7 +16,8 @@ from src.constants import BOARD_DIM
 
 
 class Board:
-    def __init__(self, mine_count=BOARD_DIM["MINE_COUNT"], width=BOARD_DIM["BOARD_WIDTH"], height=BOARD_DIM["BOARD_HEIGHT"]):
+    def __init__(self, mine_count=BOARD_DIM["MINE_COUNT"], width=BOARD_DIM["BOARD_WIDTH"],
+                 height=BOARD_DIM["BOARD_HEIGHT"]):
         """
         This function is used to generate a mine board. Default size is 16*16
         Default mine_count is 40
@@ -58,34 +59,33 @@ class Board:
             self.chessboard[y][x].set_bomb(True)
         for x in range(self.height):
             for y in range(self.width):
-                mine = self.check(x, y)
+                mine = self.check(Point(x, y))
                 if mine == -1:
                     pass
                 else:
                     self.chessboard[y][x].set_bomb(False, mine)
 
-    def update(self, x, y, flag=False):
+    def update(self, p, flag=False):
         """
         This function is used to update a board after user selects a square
         Args:
-            x (int): This is the x-value of the square
-            y (int): This is the y-value of the square
+            p (Point): The square that being opened
             flag (boolean): This value determines if user flags a square
         Returns:
             game_continue (boolean): This determines if the game should continue
         """
         if flag:
-            self.chessboard[y][x].flag()
+            self.chessboard[p.y][p.x].flag()
             return True
 
-        if self.is_game_over(x, y):
+        if self.is_game_over(p):
             for row in self.chessboard:
                 for val in row:
                     val.open()
             return False
 
-        if self.chessboard[y][x].open():
-            self.open(x, y)
+        if self.chessboard[p.y][p.x].open():
+            self.open(p)
         return True
 
     def get_board(self):
@@ -111,98 +111,94 @@ class Board:
         return True
 
     # Auxiliary functions
-    def check(self, x, y):
+    def check(self, p):
         """
         This function is used to number each square in the mine board.
         The value of a box is the number of mines around the square.
         Or the value is -1 if the square is mine.
         Args:
-            x (int): This is the x-value of the square while iterating
-            y (int): This is the y-value of the square while iterating
+            p (Point): The square that is being opened
         Returns:
             mine (int): This returns the value of the square
         """
         mine = 0
-        if self.chessboard[y][x].is_bomb():
+        if p.is_bomb():
             mine = -1
-        elif not self.chessboard[y][x].is_bomb():
-            res = self.surround(x, y)
+        elif not p.is_bomb():
+            res = self.surround(p)
             for ele in res:
                 if self.chessboard[ele[1]][ele[0]].is_bomb():
                     mine += 1
         return mine
 
-    def open(self, x, y):
+    def open(self, p):
         """
         This function is used to update the board when a 0-value
         point is opened
         Args:
-            x (int): This is the x-value of the box
-            y (int): This is the y-value of the box
+           p (Point): The point being opened
         Returns:
             void
         """
-        self.chessboard[y][x].open()
-        for p in self.surround(x, y):
-            point = self.chessboard[p[1]][p[0]]
+        p.open()
+        for s in self.surround(p):
+            point = self.chessboard[s[1]][s[0]]
             if point.is_bomb():
                 continue
             if point.open():
-                self.open(point.point_data.x, point.point_data.y)
+                self.open(Point(s[0], s[1]))
 
-    def surround(self, x, y):
+    def surround(self, p):
         """
         This function is used to visit boxes around (x, y)
         It would return a set of boxes' coordinates
         Args:
-            x (int): This is the x-value of the box
-            y (int): This is the y-value of the box
+            p (Point): The point being opened
         Returns:
             res(set((int,int))): This is a set of coordinates of all nearby boxes
         """
         res = set([])
-        if x + 1 < self.height:
-            res.add((x + 1, y))
-            if y + 1 < self.width:
-                res.add((x + 1, y + 1))
-                res.add((x, y + 1))
-            if y - 1 >= 0:
-                res.add((x + 1, y - 1))
-                res.add((x, y - 1))
-        if x - 1 >= 0:
-            res.add((x - 1, y))
-            if y + 1 < self.width:
-                res.add((x - 1, y + 1))
-                res.add((x, y + 1))
-            if y - 1 >= 0:
-                res.add((x - 1, y - 1))
-                res.add((x, y - 1))
+        if p.x + 1 < self.height:
+            res.add((p.x + 1, p.y))
+            if p.y + 1 < self.width:
+                res.add((p.x + 1, p.y + 1))
+                res.add((p.x, p.y + 1))
+            if p.y - 1 >= 0:
+                res.add((p.x + 1, p.y - 1))
+                res.add((p.x, p.y - 1))
+        if p.x - 1 >= 0:
+            res.add((p.x - 1, p.y))
+            if p.y + 1 < self.width:
+                res.add((p.x - 1, p.y + 1))
+                res.add((p.x, p.y + 1))
+            if p.y - 1 >= 0:
+                res.add((p.x - 1, p.y - 1))
+                res.add((p.x, p.y - 1))
         return res
 
-    def is_game_over(self, x, y):
+    def is_game_over(self, p):
         """
         This function is used to determine if the user selects a mine
         Args:
-            x (int): This is the x-value of the selected square
-            y (int): This is the y-value of the selected square
+            p (Point): The square being opened
         Returns:
             True, if it is a bomb
             False, otherwise
         """
-        return self.chessboard[y][x].is_bomb()
+        return self.chessboard[p.y][p.x].is_bomb()
 
 
 # Section below is for test use
 
 def test():
-    mineboard = Board()
+    mineboard = Board(1, 4)
     for row in mineboard.chessboard:
         for val in row:
             if val.point_data.is_bomb:
                 val.point_data.bomb_around = '*'
             print(val.point_data.bomb_around, end=' ')
         print()
-    mineboard.update(5, 5)
+    mineboard.update(Point(1, 0))
     for row in mineboard.chessboard:
         for val in row:
             print(val.point_data.is_opened, end=' ')
