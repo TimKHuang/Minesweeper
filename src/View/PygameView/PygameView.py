342fad3b70b06c78aae7c7a02dce8e78633d8248
Victorvv <55767941+Victor-1024@users.noreply.github.com>
2020-07-17 15:38:34 +0800

    Improve the code

--- src/View/PygameView/PygameView.py
+++ src/View/PygameView/PygameView.py
@@ -13,128 +13,150 @@ from src.constants import RGB
 from src.Button import Button
 from src.View import View
 
+CELL_WIDTH = 20
+CELL_HEIGHT = 20
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 50
+BOARD_WIDTH = 400
+BOARD_HEIGHT = 600
 
-class PygameView:
+
+def get_board_size(screen, event):
     """
-    Showing the board as GUI
-    Attributes:
-        width = width of the screen
-        height = height of the screen
-        screen = the screen to display the game
+    This function is used initialise the game
+    And get the board size of the game
+    Args:
+        screen(String): the screen user wants to display
+        event(Event): this is user's action
+    Returns:
+    game_level(dictionary): This determines the dimension of the board
+        """
+    # Set up window and background
+    screen.fill(RGB["WHITE"])
+    pygame.display.set_caption('MineSweeper')
+
+    # Dimension of button
+    font_size = 20
+    all_button_height = 350
+    # Set up the option buttons
+    beginner_button = Button(
+        (BOARD_WIDTH// 2 - BUTTON_WIDTH // 2, (BOARD_HEIGHT - all_button_height) // 2, BUTTON_WIDTH, BUTTON_HEIGHT),
+        RGB["PALE_GREEN"], "Beginner", RGB["WHITE"], font_size)
+    beginner_button.draw_button(screen)
+    intermediate_button = Button((BOARD_WIDTH // 2 - BUTTON_WIDTH // 2, (BOARD_HEIGHT - all_button_height) // 2 + 100,
+                                  BUTTON_WIDTH, BUTTON_HEIGHT),
+                                 RGB["PALE_GREEN"], "Intermediate", RGB["WHITE"], font_size)
+    intermediate_button.draw_button(screen)
+    customise_button = Button((BOARD_WIDTH // 2 - BUTTON_WIDTH // 2, (BOARD_HEIGHT - all_button_height) // 2 + 200,
+                               BUTTON_WIDTH, BUTTON_HEIGHT),
+                              RGB["PALE_GREEN"], "Customise", RGB["WHITE"], font_size)
+    customise_button.draw_button(screen)
+    # First time set up the screen
+    pygame.display.flip()
+
+    # Get the game level
+    while True:
+        if beginner_button.update_button(event, screen):
+            pygame.display.update()
+            return beginner_button.update_button(event, screen)
+
+        elif intermediate_button.update_button(event, screen):
+            pygame.display.update()
+            return intermediate_button.update_button(event, screen)
+
+        else:
+            pygame.display.update()
+            return customise_button.update_button(event, screen)
+
+
+def draw(screen, board):
     """
+    This function is used to draw the board, and update the board
+    Args:
+        screen(String): the screen user wants to display
+        board(matrix of pointdata): the mine board
+    Returns:
+        void
+    """
+    screen.fill(RGB["WHITE"])
 
-    def __init__(self, width=400, height=600):
-        """
-        Constructor for the GUI
-        Args:
-            width: the width of the screen
-            height: the height of the screen
-        """
-        self.width = width
-        self.height = height
-        screen = pygame.display.set_mode((self.width, self.height))
-        self.screen = screen
+    # Get the dimension of the board
+    rows = len(board)
+    cols = len(board[0])
+    for y in range(rows):
+        # Draw the y axis
+        pygame.draw.line(screen, RGB["CYANINE"], (0, CELL_HEIGHT * y), (BOARD_WIDTH, CELL_HEIGHT * y), 5)
+        for x in range(cols):
+            # Draw the x axis
+            pygame.draw.line(screen, RGB["CYANINE"], (CELL_WIDTH * x, 0), (CELL_WIDTH * x, BOARD_HEIGHT), 5)
+            point = board[y][x]
+            if point.is_opened:
+                # Load the image of mine to the screen
+                if point.is_bomb:
+                    bomb = pygame.image.load('material/mine.jpg')
+                    bomb = pygame.transform.smoothscale(bomb, (CELL_WIDTH, CELL_HEIGHT))
+                    screen.blit(bomb, (x * CELL_WIDTH + 5, y * CELL_HEIGHT + 5))
+                    continue
+                # Draw the point out
+                bx = x * CELL_WIDTH -5
+                by = y * CELL_HEIGHT - 5
+                bw = CELL_WIDTH - 5
+                bh = CELL_HEIGHT - 5
+                pygame.draw.rect(screen, RGB["HAZE"], (bx, by, bw, bh), 5)
+                number_font = pygame.font.Font('material/number.ttf', 10)
+                number = number_font.render(point, True, RGB[str(point)])
+                tw, th = number.get_size()
+                # Centralise the number
+                tx = bx + bw / 2 - tw / 2
+                ty = by + bh / 2 - th / 2
+                screen.blit(number, (tx, ty))
 
-    def get_board_size(self, event):
-        """
-        This function is used initialise the game
-        And get the board size of the game
-        Args:
-            event:
+            if point.is_flagged:
+                # Load the image of flag to the screen
+                flag = pygame.image.load('material/flag.png')
+                flag = pygame.transform.smoothscale(flag, (CELL_WIDTH, CELL_HEIGHT))
+                screen.blit(flag, (x * CELL_WIDTH + 5, y * CELL_HEIGHT + 5))
+                continue
 
-        Returns:
+            # Draw squares for unopened point on the board
+            bx = x * CELL_WIDTH - 5
+            by = y * CELL_HEIGHT - 5
+            bw = CELL_WIDTH - 5
+            bh = CELL_HEIGHT - 5
+            pygame.draw.rect(screen, RGB["PALE_GREEN"], (bx, by, bw, bh))
 
-        """
-        # Initialise the game
-        pygame.init()
+    # Update the screen after changing
+    pygame.display.update()
 
-        # Set up window and background
-        self.screen.fill(RGB["WHITE"])
-        pygame.display.set_caption('MineSweeper')
 
-        # Dimension of button
-        font_size = 20
-        button_width = 200
-        button_height = 50
-        all_button_height = 350
-        # Set up the option buttons
-        beginner_button = Button(
-            (self.width // 2 - button_width // 2, (self.height - all_button_height) // 2, button_width, button_height),
-            RGB["PALE_GREEN"], "Beginner", RGB["WHITE"], font_size)
-        beginner_button.draw_button(self.screen)
-        intermediate_button = Button((self.width // 2 - button_width // 2, (self.height - all_button_height) // 2 + 100,
-                                      button_width, button_height),
-                                     RGB["PALE_GREEN"], "Intermediate", RGB["WHITE"], font_size)
-        intermediate_button.draw_button(self.screen)
-        customise_button = Button((self.width // 2 - button_width // 2, (self.height - all_button_height) // 2 + 200,
-                                   button_width, button_height),
-                                  RGB["PALE_GREEN"], "Customise", RGB["WHITE"], font_size)
-        customise_button.draw_button(self.screen)
-        pygame.display.flip()
+class UserPlayer:
+    """
+    A simple user player that reads moves
+    """
+    user_move = {
+        "POS": (),
+        "OPEN": False,
+        "FLAG": False
+    }
 
-        # Get the game level
+    def choose_action(self, board):
         while True:
-            if beginner_button.update_button(event, self.screen):
-                pygame.display.update()
-                board_size = beginner_button.update_button(event, self.screen)
-                return board_size
-                break
-            elif intermediate_button.update_button(event, self.screen):
-                pygame.display.update()
-                board_size = intermediate_button.update_button(event, self.screen)
-                return board_size
-            else:
-                pygame.display.update()
-                board_size = customise_button.update_button(event, self.screen)
-                return board_size
-
-    def draw(self, board):
-        """
-        This function is used to draw the board, and update the board
-        Args:
-            board: the mine board
-        Returns:
-            void
-        """
-        # Get the dimension of the board
-        rows = len(board)
-        cols = len(board[0])
-        # square dimension
-        sqr_width = self.width / cols
-        sqr_height = self.height / rows
-        for y in range(rows):
-            # Draw the y axis
-            pygame.draw.line(self.screen, RGB["CYANINE"], (0, sqr_height * y), (self.width, sqr_height * y), 5)
-            for x in range(cols):
-                # Draw the x axis
-                pygame.draw.line(self.screen, RGB["CYANINE"], (sqr_width * x, 0), (sqr_width * x, self.height), 5)
-                # Draw the square
-                square = Button((x * sqr_width + 5, y * sqr_height + 5, sqr_width - 5, sqr_height - 5), RGB["HAZE"])
-                square.draw_square(self.screen)
-        pygame.display.update()
-
-    def detect_event(self, event):
-        """
-        This function is used to update the GUI according to the event
-        Args:
-            event(Event): user's event
-
-        Returns:
-            void
-        """
-        if event.type == pygame.QUIT:
-            exit()
-
-        # Choose the buttons
-
-    def run(self):
-        """
-        A function runs the game
-        Returns:
-
-        """
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                raise SystemExit
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                self.user_move["POS"] = event.pos
+                self.user_move["OPEN"] = True
+                self.user_move["FLAG"] = False
+                return self.user_move
+            elif event.type == pygame and event.button == 3:
+                self.user_move["POS"] = event.pos
+                self.user_move["OPEN"] = False
+                self.user_move["FLAG"] = True
+                return self.user_move
 
 
+# To be added
 #
 # while True:
 #
@@ -153,23 +175,7 @@ class PygameView:
 # The section below is for test purposes
 
 def test():
-    from src.Board import Board
-    view = PygameView()
-    board_size = None
-    while True and board_size is None:
-        for event in pygame.event.get():
-            view.detect_event(event)
-            if view.get_board_size(event):
-                print(view.get_board_size(event))
-                board_size = view.get_board_size(event)
-                break
-    view.screen.fill(RGB["WHITE"])
-    board = Board(1, 2, 2).get_board()
-    view.draw(board)
-    while True:
-        for event in pygame.event.get():
-            view.detect_event(event)
-
+    pass
 
 if __name__ == '__main__':
     test()
