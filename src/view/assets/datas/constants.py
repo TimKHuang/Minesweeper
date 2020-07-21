4e23dcea9c2f614e04b84d862d6dfad7fa266747
Victorvv <55767941+Victor-1024@users.noreply.github.com>
2020-07-21 17:27:17 +0800

    Correct the file

--- src/view/assets/datas/constants.py
+++ src/view/assets/datas/constants.py
@@ -7,6 +7,26 @@
 @time: 2020/7/9 下午6:45
 @description: This file contains datas value for minesweeper
 """
+BOARD_DIM = {"MINE_COUNT": 40,
+             "BOARD_WIDTH": 16,
+             "BOARD_HEIGHT": None
+             }
+
+# The dictionary below denotes the colour for each square's value
+COLOUR = {"B": "\033[1m",
+          "F": "\033[4m",
+          "*": "\033[0m",
+          0: "\033[0m",
+          1: "\033[30m",
+          2: "\033[31m",
+          3: "\033[32m",
+          4: "\033[33m",
+          5: "\033[34m",
+          6: "\033[35m",
+          7: "\033[36m",
+          8: "\033[37m",
+          }
+
 # The dictionary defines the RGB value for different colours
 RGB = {"PALE_GREEN": (199, 237, 233),
        "WHITE": (255, 255, 255),
