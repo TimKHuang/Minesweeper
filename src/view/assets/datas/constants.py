87688432cb5954789598c476c5fd433630a55170
Victorvv <55767941+Victor-1024@users.noreply.github.com>
2020-07-21 17:22:54 +0800

    Chang the game level

--- src/view/assets/datas/constants.py
+++ src/view/assets/datas/constants.py
@@ -7,38 +7,32 @@
 @time: 2020/7/9 下午6:45
 @description: This file contains datas value for minesweeper
 """
-BOARD_DIM = {"MINE_COUNT": 40,
-             "BOARD_WIDTH": 16,
-             "BOARD_HEIGHT": None
-             }
-
-# The dictionary below denotes the colour for each square's value
-COLOUR = {"B": "\033[1m",
-          "F": "\033[4m",
-          "*": "\033[0m",
-          0: "\033[0m",
-          1: "\033[30m",
-          2: "\033[31m",
-          3: "\033[32m",
-          4: "\033[33m",
-          5: "\033[34m",
-          6: "\033[35m",
-          7: "\033[36m",
-          8: "\033[37m",
-          }
-
 # The dictionary defines the RGB value for different colours
 RGB = {"PALE_GREEN": (199, 237, 233),
        "WHITE": (255, 255, 255),
        "HAZE": (175, 215, 237),
-       "CYANINE": (92, 167, 186)
+       "CYANINE": (92, 167, 186),
+       "1": (225, 238, 210),
+       "2": (219, 208, 167),
+       "3": (230, 155, 3),
+       "4": (209, 73, 78),
+       "5": (18, 53, 85),
+       "6": (227, 179, 37),
+       "7": (119, 52, 96),
+       "8": (78, 29, 76)
 }
 
 # The dictionary defines the dimension of the board for Intermediate
 INTERMEDIATE_BOARD = {
-    "MINE_COUNT": 99,
-    "BOARD_WIDTH": 16,
-    "BOARD_HEIGHT": 30
+    "mine_count": 40,
+    "width": 16,
+    "height": 16
+}
+
+BEGINNER_BOARD = {
+    "mine_count": 10,
+    "width": 8,
+    "height": 8
 }
 
 # The dictionary defines the level of the game
