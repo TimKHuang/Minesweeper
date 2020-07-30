dfc4f6a1635242545b73e89e9386f84591336b49
Victorvv <55767941+Victor-1024@users.noreply.github.com>
2020-07-30 17:31:22 +0800

    Add bg and can show number of remained mine

--- src/view/PygameView/PygameView.py
+++ src/view/PygameView/PygameView.py
@@ -62,28 +62,31 @@ class PygameView(View):
         pygame.display.set_caption("Minesweeper")
         icon = pygame.image.load('view/assets/images/icon.jpg').convert_alpha()
         pygame.display.set_icon(icon)
-        # Each iteration needs to fill the screen white again
-        self.screen.fill(RGB["WHITE"])
+
+        # Each iteration needs to refill the screen
+        bg = pygame.image.load('view/assets/images/bg-galaxy.jpg').convert_alpha()
+        bg = pygame.transform.smoothscale(bg, (self.screen_width, self.screen_height))
+        self.screen.blit(bg, (0, 0))
 
         # Set up beginner button
         beginner_button = Button(
-            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2, BUTTON_WIDTH,
+            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2 + 50, BUTTON_WIDTH,
              BUTTON_HEIGHT),
-            RGB["PALE_GREEN"], "Beginner", RGB["WHITE"], MESSAGE_SIZE)
+            RGB["VIOLET"], "Beginner", RGB["WHITE"], MESSAGE_SIZE)
         beginner_button.draw_button(self.screen)
 
         # Set up intermediate button
         intermediate_button = Button(
-            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2 + 100,
+            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2 + 150,
              BUTTON_WIDTH, BUTTON_HEIGHT),
-            RGB["PALE_GREEN"], "Intermediate", RGB["WHITE"], MESSAGE_SIZE)
+            RGB["VIOLET"], "Intermediate", RGB["WHITE"], MESSAGE_SIZE)
         intermediate_button.draw_button(self.screen)
 
         # Set up customise button
         customise_button = Button(
-            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2 + 200,
+            (self.screen_width // 2 - BUTTON_WIDTH // 2, (self.screen_height - all_button_height) // 2 + 250,
              BUTTON_WIDTH, BUTTON_HEIGHT),
-            RGB["PALE_GREEN"], "Customise", RGB["WHITE"], MESSAGE_SIZE)
+            RGB["VIOLET"], "Customise", RGB["WHITE"], MESSAGE_SIZE)
         customise_button.draw_button(self.screen)
 
         # First time set up the screen
@@ -105,9 +108,12 @@ class PygameView(View):
             if customise_button.within_bound(pygame.mouse.get_pos()):
                 if customise_button.update_button(self.event, self.screen):
                     self.screen.fill(RGB["WHITE"])
-                    beginner_button.set_alpha(self.screen, 10)
-                    intermediate_button.set_alpha(self.screen, 10)
-                    customise_button.set_alpha(self.screen, 10)
+                    bg = pygame.image.load('view/assets/images/bg-galaxy.jpg').convert_alpha()
+                    bg = pygame.transform.smoothscale(bg, (self.screen_width, self.screen_height))
+                    self.screen.blit(bg, (0, 0))
+                    beginner_button.set_alpha(self.screen, 50)
+                    intermediate_button.set_alpha(self.screen, 50)
+                    customise_button.set_alpha(self.screen, 50)
                     pygame.display.update()
                     board = self._draw_text_box(MESSAGE_SIZE)
 
@@ -128,35 +134,34 @@ class PygameView(View):
         """
         board = {}
         font = pygame.font.Font('view/assets/fonts/Trinity.ttf', size)
-
-        mine_text = font.render('Mine count ', True, RGB["HAZE"])
+        mine_text = font.render('Mine count ', True, RGB["VIOLET"])
         mine_tw, mine_th = mine_text.get_size()
         mine_tx = (self.screen_width - mine_tw) // 6
         mine_ty = (self.screen_height - mine_th) // 6
-        self.screen.blit(mine_text, (mine_tx, mine_ty))
+        self.screen.blit(mine_text, (mine_tx, mine_ty + 50))
         mine_count_box = TextBox(
-            (mine_tx + mine_tw + 20, mine_ty - 10, self.screen_width - mine_tw - mine_tx - 40, mine_th * 2), size,
-            RGB["HAZE"])
+            (mine_tx + mine_tw + 20, mine_ty + 40, self.screen_width - mine_tw - mine_tx - 40, mine_th * 2), size,
+            RGB["VIOLET"])
         mine_count_box.draw(self.screen)
 
-        width_text = font.render('Width ', True, RGB["HAZE"])
+        width_text = font.render('Width ', True, RGB["VIOLET"])
         width_tw, width_th = width_text.get_size()
         width_tx = mine_tx
         width_ty = mine_ty + 100
-        self.screen.blit(width_text, (width_tx, width_ty))
+        self.screen.blit(width_text, (width_tx, width_ty + 50))
         width_box = TextBox(
-            (mine_tx + mine_tw + 20, width_ty - 10, self.screen_width - mine_tw - mine_tx - 40, width_th * 2),
-            size, RGB["HAZE"])
+            (mine_tx + mine_tw + 20, width_ty + 40, self.screen_width - mine_tw - mine_tx - 40, width_th * 2),
+            size, RGB["VIOLET"])
         width_box.draw(self.screen)
 
-        height_text = font.render('Height ', True, RGB["HAZE"])
+        height_text = font.render('Height ', True, RGB["VIOLET"])
         height_tw, height_th = height_text.get_size()
         height_tx = mine_tx
         height_ty = width_ty + 100
-        self.screen.blit(height_text, (height_tx, height_ty))
+        self.screen.blit(height_text, (height_tx, height_ty + 50))
         height_box = TextBox(
-            (mine_tx + mine_tw + 20, height_ty - 10, self.screen_width - mine_tw - mine_tx - 40, height_th * 2),
-            size, RGB["HAZE"])
+            (mine_tx + mine_tw + 20, height_ty + 40, self.screen_width - mine_tw - mine_tx - 40, height_th * 2),
+            size, RGB["VIOLET"])
         height_box.draw(self.screen)
 
         pygame.display.update()
@@ -187,19 +192,19 @@ class PygameView(View):
 
                 elif event.type == pygame.QUIT:
                     raise SystemExit
+
             pygame.time.delay(33)
             mine_count_box.draw(self.screen)
             width_box.draw(self.screen)
             height_box.draw(self.screen)
             pygame.display.update()
 
-    def draw(self, board):
+    def draw(self, board, remaining):
         """
         This function is used to draw the board, and update the board
         Args:
             board: the selected board
-        Returns:
-            void
+            remaining (int): The number of remained mine on the board
         """
         # Dimension of cell
         CELL_WIDTH = 30
@@ -218,19 +223,29 @@ class PygameView(View):
         startpos_y = (self.screen_height - max_height) // 2
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.RESIZABLE)
 
-        # During each iteration the prev screen is filled white
-        self.screen.fill(RGB["WHITE"])
+        # During each iteration the prev screen is refilled
+        bg = pygame.image.load('view/assets/images/bg-yourname.jpg').convert_alpha()
+        bg = pygame.transform.smoothscale(bg, (self.screen_width, self.screen_height))
+        bg.set_alpha(10)
+        self.screen.blit(bg, (0, 0))
 
         for y in range(self.board_rows):
             # Draw the x axis
             pygame.draw.line(self.screen, RGB["BLACK"], (startpos_x, CELL_HEIGHT * y + startpos_y),
                              (max_width, CELL_HEIGHT * y + startpos_y),
                              LINE_WIDTH)
+
         for x in range(self.board_cols):
             # Draw the y axis
             pygame.draw.line(self.screen, RGB["BLACK"], (CELL_WIDTH * x + startpos_x, startpos_y),
                              (CELL_WIDTH * x + startpos_x, max_height),
                              LINE_WIDTH)
+
+        font = pygame.font.Font('view/assets/fonts/digital-7.ttf', 40)
+        text = "Mine remained: " + str(remaining)
+        remained_mine = font.render(text, True, RGB["PALE_PURPLE"])
+        remained_mine_w, remained_mine_h = remained_mine.get_size()
+        self.screen.blit(remained_mine, (self.screen_width - remained_mine_w, 0))
 
         # Draw the board out
         for y in range(self.board_rows):
@@ -239,8 +254,8 @@ class PygameView(View):
                 if point.is_opened:
                     # Load the image of mine to the screen
                     if point.is_bomb:
-                        bomb = pygame.image.load('view/assets/images/mine.jpg')
-                        bomb = pygame.transform.smoothscale(bomb, (CELL_WIDTH - 2, CELL_HEIGHT - 2))
+                        bomb = pygame.image.load('view/assets/images/mine.png')
+                        bomb = pygame.transform.smoothscale(bomb, (CELL_WIDTH - 5, CELL_HEIGHT - 5))
                         self.screen.blit(bomb, (x * CELL_WIDTH + startpos_x, y * CELL_HEIGHT + startpos_y))
                         continue
 
@@ -266,7 +281,7 @@ class PygameView(View):
                 if point.is_flagged:
                     # Load the image of flag to the screen
                     flag = pygame.image.load('view/assets/images/flag.png')
-                    flag = pygame.transform.smoothscale(flag, (CELL_WIDTH - 2, CELL_HEIGHT - 2))
+                    flag = pygame.transform.smoothscale(flag, (CELL_WIDTH - 5, CELL_HEIGHT - 5))
                     self.screen.blit(flag, (x * CELL_WIDTH + startpos_x, y * CELL_HEIGHT + startpos_y))
                     continue
 
@@ -276,7 +291,7 @@ class PygameView(View):
                     by = y * CELL_HEIGHT + startpos_y
                     bw = CELL_WIDTH - 2
                     bh = CELL_HEIGHT - 2
-                    pygame.draw.rect(self.screen, RGB["PALE_GREEN"], (bx, by, bw, bh))
+                    pygame.draw.rect(self.screen, RGB["SAKURA"], (bx, by, bw, bh))
                     continue
 
         # Update the screen after changing
@@ -295,12 +310,19 @@ class PygameView(View):
         ty = by + bh / 2 - th / 2
         self.screen.blit(text, (tx, ty))
 
-    def fail(self):
+    def _get_continue(self, message):
         """
-        Deal with the situation when game fails
+        This function is used get if user wants to continue the game
+        Args:
+            message: The message to be displayed
         Returns:
-            continue(boolean): True is start. False otherwise
+            continue (boolean): if user wants to continue the game
         """
+        """
+                Deal with the situation when game fails
+                Returns:
+                    continue(boolean): True is start. False otherwise
+                """
         # Dimension of the button
         DEMAND_SIZE = 30
         MESSAGE_SIZE = 20
@@ -314,26 +336,26 @@ class PygameView(View):
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.reset_timer()
 
-        # Each iteration needs to fill the window white
-        self.screen.fill(RGB["WHITE"])
-        pygame.display.update()
+        # Refill the screen
+        bg = pygame.image.load('view/assets/images/bg-space.jpg').convert_alpha()
+        bg = pygame.transform.smoothscale(bg, (self.screen_width, self.screen_height))
+        self.screen.blit(bg, (0, 0))
 
         bx, by, bw, bh = (
             self.screen_width / 6, self.screen_height / 4, self.screen_width * 2 / 3, self.screen_height / 6)
-        pygame.draw.rect(self.screen, RGB["PALE_GREEN"], (bx, by, bw, bh))
         # Get the font
         font = pygame.font.Font('view/assets/fonts/Trinity.ttf', DEMAND_SIZE)
-        text = font.render("You've met a bomb", True, RGB["WHITE"])
+        text = font.render(message, True, RGB["PALE_PURPLE"])
         self._centralise(text, (bx, by, bw, bh))
 
         # Create two buttons
         restart_button = Button(
             (self.screen_width / 6, self.screen_height / 2, self.screen_width / 6, self.screen_height / 10),
-            RGB["PALE_GREEN"],
+            RGB["PALE_PURPLE"],
             "RESTART", RGB["WHITE"], MESSAGE_SIZE)
         quit_button = Button(
             (self.screen_width * 2 / 3, self.screen_height / 2, self.screen_width / 6, self.screen_height / 10),
-            RGB["PALE_GREEN"],
+            RGB["PALE_PURPLE"],
             "QUIT", RGB["WHITE"], MESSAGE_SIZE)
         restart_button.draw_button(self.screen)
         quit_button.draw_button(self.screen)
@@ -354,6 +376,14 @@ class PygameView(View):
 
             if self.event.type == pygame.QUIT:
                 raise SystemExit
+
+    def fail(self):
+        """
+        Deal with the situation when game fails
+        Returns:
+            continue(boolean): True is start. False otherwise
+        """
+        return self._get_continue("You've met a bomb")
 
     def win(self):
         """
@@ -361,57 +391,7 @@ class PygameView(View):
         Returns:
             continue(boolean): True is start. False otherwise
         """
-        # Dimension of the button
-        DEMAND_SIZE = 30
-        MESSAGE_SIZE = 20
-
-        # The dimension of this window is fixed:
-        self.screen_width = 600
-        self.screen_height = 600
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        self.reset_timer()
-
-        # Each iteration needs to fill the window white
-        self.screen.fill(RGB["WHITE"])
-        pygame.display.update()
-
-        bx, by, bw, bh = (
-            self.screen_width / 6, self.screen_height / 4, self.screen_width * 2 / 3, self.screen_height / 6)
-        pygame.draw.rect(self.screen, RGB["PALE_GREEN"], (bx, by, bw, bh))
-
-        # Get the font
-        font = pygame.font.Font('view/assets/fonts/Trinity.ttf', DEMAND_SIZE)
-        text = font.render("Wow! Excellent", True, RGB["WHITE"])
-        self._centralise(text, (bx, by, bw, bh))
-
-        # Create two buttons
-        restart_button = Button(
-            (self.screen_width / 6, self.screen_height / 2, self.screen_width / 6, self.screen_height / 10),
-            RGB["PALE_GREEN"],
-            "RESTART", RGB["WHITE"], MESSAGE_SIZE)
-        quit_button = Button(
-            (self.screen_width * 2 / 3, self.screen_height / 2, self.screen_width / 6, self.screen_height / 10),
-            RGB["PALE_GREEN"],
-            "QUIT", RGB["WHITE"], MESSAGE_SIZE)
-        restart_button.draw_button(self.screen)
-        quit_button.draw_button(self.screen)
-        pygame.display.update()
-
-        # Get user's choice
-        while True:
-            self.event = pygame.event.wait()
-            if restart_button.within_bound(pygame.mouse.get_pos()):
-                if restart_button.update_button(self.event, self.screen):
-                    pygame.display.update()
-                    return restart_button.get_choice()
-
-            if quit_button.within_bound(pygame.mouse.get_pos()):
-                if quit_button.update_button(self.event, self.screen):
-                    pygame.display.update()
-                    return quit_button.get_choice()
-
-            if self.event.type == pygame.QUIT:
-                raise SystemExit
+        return self._get_continue("Wow! Excellent!")
 
     def _get_board_coordinates(self, pos):
         """
@@ -446,12 +426,16 @@ class PygameView(View):
         This function is used to update the time
         """
         time_passed = "TIME: " + str(int(self.time_running())) + " SEC"
-        font = pygame.font.Font('view/assets/fonts/digital-7.ttf', 50)
-        text = font.render(time_passed, True, RGB["BLACK"])
-        time_rect = pygame.Surface(text.get_size())
-        time_rect.fill(RGB["WHITE"])
-        self.screen.blit(time_rect, (0, 0))
+        font = pygame.font.Font('view/assets/fonts/digital-7.ttf', 40)
+        text = font.render(time_passed, True, RGB["PALE_PURPLE"])
         self.screen.blit(text, (0, 0))
+
+    def _reload_screen(self):
+        """
+        This function is used to reload the screen
+        """
+        snapshot = pygame.image.load('view/assets/images/bg-yourname_snap.jpg')
+        self.screen.blit(snapshot, (0, 0))
 
     def input(self):
         """
@@ -460,7 +444,6 @@ class PygameView(View):
             user_move{"POS": (int, int), "OPEN": boolean, "FLAG": bool}
         """
         user_move = {}
-
         while True:
             for event in pygame.event.get():
                 self.event = event
@@ -481,6 +464,7 @@ class PygameView(View):
                         user_move["y"] = coordinates[1]
                         return user_move
             # Update the time taken
+            self._reload_screen()
             self._update_time()
             pygame.display.update()
 
